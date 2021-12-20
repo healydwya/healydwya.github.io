3dcf8cb30e1cbeb78f86cd77b8752537eff7550a
Healy Dwyer <healydwyer@gmail.com>
2021-12-20 18:17:26 -0500

    updated resume and about page

--- src/About.js
+++ src/About.js
@@ -8,10 +8,11 @@ function About(props) {
       <div className="about-text">
         <img src={bike} className="App-logo" alt="logo" />
         <div className="col">
-          <p> With 4+ years of experience as a UX researcher + engineer and a background in urban planning, my passion is building technology to make the world more equitable. </p>
-          <p>I am currently pursuing my master's degree in Human-Computer Interaction at Carnegie Mellon University and will be open to full-time roles following graduation in August of 2022. </p>
-          <p> Technology-wise, progressive web apps get my blood pumping, and I enjoy working with JavaScript frameworks such as React and Angular, delving into the world of human-robot interaction, and conducting user research to help build better products. </p>
-          <p> Outside of work and school, I founded a transportation advocacy organization called <a href="http://instagram.com/bikeduval" target="_blank" rel="noreferrer"> Bike Duval </a>, served as the president of the Jacksonville Bicycle and Pedestrian Advisory Committee, and volunteer my skills doing React Native development for the app <a href="http://bikelaneuprising.com" target="_blank" rel="noreferrer"> Bike Lane Uprising. </a> You can usually find me running, cycling, hiking, camping, hanging with pals, and drinking coffee around town. </p>
+          <p> With <b>4+ years of experience as a UX researcher + engineer </b> along with a background in urban planning, my passion is building technology to improve the systems that shape our world. </p>
+          <p> Having worked as both a software engineer and researcher, my strengths lie in <b> technical HCI</b> and <b> research through prototyping. </b>I bring a unique perspective to communicating insights and brainstorming solutions with product managers, designers, researchers, and engineers. </p>
+          <p>I am currently pursuing my master's degree in <b> Human-Computer Interaction</b> at <b>Carnegie Mellon University</b> and will be open to full-time roles following graduation in August of 2022. </p>
+          <p> Outside of work and school, I founded a transportation advocacy organization called <a href="http://instagram.com/bikeduval" target="_blank" rel="noreferrer"> Bike Duval </a>, served as the president of the <a href="https://www.facebook.com/bpacfriends" target="_blank" rel="noreferrer">Jacksonville Bicycle and Pedestrian Advisory Committee </a>, and previously volunteered my skills doing React Native development for the app <a href="http://bikelaneuprising.com" target="_blank" rel="noreferrer"> Bike Lane Uprising. </a> </p>
+          <p> You can usually find me biking with pals, camping, or playing fetch with my big fluffy dog, Otis.</p>
         </div>
       </div>
     </div>
