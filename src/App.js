85549ba252fe311e2be55e4918c25580742b86af
Healy Dwyer <healydwyer@gmail.com>
2022-03-01 12:48:39 -0500

    resume update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Ucre from './Ucre/Ucre';
 import HRI from './HRI/HRI';
 import Lib from './Lib/Lib';
 import CSX from './CSX/CSX';
-import resume from './Healy_Dwyer_2021_resume.pdf';
+import resume from './HealyDwyerSpring22Resume.pdf';
 import Footer from './Footer';
 
 class App extends React.Component {
